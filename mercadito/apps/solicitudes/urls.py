bd865ca6bdad98f292c6901e1324af4de138046b
InsaneCamilo <superdantelolol@gmai.com>
2026-05-11 14:21:18 -0500

    Modulo solicitudes

---
+++ mercadito/apps/solicitudes/urls.py
@@ -0,0 +1,35 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path(
+        'enviar/',
+        views.enviar_solicitud,
+        name='enviar_solicitud'
+    ),
+
+    path(
+        'lista/',
+        views.lista_solicitudes,
+        name='lista_solicitudes'
+    ),
+
+    path(
+        'aceptar/<str:solicitud_id>/',
+        views.aceptar_solicitud,
+        name='aceptar_solicitud'
+    ),
+
+    path(
+        'rechazar/<str:solicitud_id>/',
+        views.rechazar_solicitud,
+        name='rechazar_solicitud'
+    ),
+
+    path(
+        'cancelar/<str:solicitud_id>/',
+        views.cancelar_solicitud,
+        name='cancelar_solicitud'
+    ),
+]
--- mercadito/apps/solicitudes/views.py
+++ mercadito/apps/solicitudes/views.py
@@ -1,3 +1,95 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
 
-# Create your views here.
+from .models import SolicitudIntercambio
+from .forms import SolicitudIntercambioForm
+
+
+def enviar_solicitud(request):
+
+    if request.method == 'POST':
+
+        form = SolicitudIntercambioForm(request.POST)
+
+        if form.is_valid():
+
+            solicitud = form.save(commit=False)
+
+            solicitud.publicacion = {
+                "titulo": form.cleaned_data['titulo_publicacion'],
+                "categoria": form.cleaned_data['categoria_publicacion']
+            }
+
+            solicitud.solicitante = {
+                "nombre": form.cleaned_data['nombre_solicitante'],
+                "correo": form.cleaned_data['correo_solicitante']
+            }
+
+            solicitud.save()
+
+            return redirect('lista_solicitudes')
+
+    else:
+
+        form = SolicitudIntercambioForm()
+
+    return render(
+        request,
+        'solicitudes/enviar_solicitud.html',
+        {'form': form}
+    )
+
+
+def lista_solicitudes(request):
+
+    solicitudes = SolicitudIntercambio.objects.all()
+
+    return render(
+        request,
+        'solicitudes/lista_solicitudes.html',
+        {'solicitudes': solicitudes}
+    )
+
+
+def aceptar_solicitud(request, solicitud_id):
+
+    solicitud = get_object_or_404(
+        SolicitudIntercambio,
+        id=solicitud_id
+    )
+
+    solicitud.estado_solicitud = 'aceptada'
+    solicitud.fecha_respuesta = timezone.now()
+
+    solicitud.save()
+
+    return redirect('lista_solicitudes')
+
+
+def rechazar_solicitud(request, solicitud_id):
+
+    solicitud = get_object_or_404(
+        SolicitudIntercambio,
+        id=solicitud_id
+    )
+
+    solicitud.estado_solicitud = 'rechazada'
+    solicitud.fecha_respuesta = timezone.now()
+
+    solicitud.save()
+
+    return redirect('lista_solicitudes')
+
+
+def cancelar_solicitud(request, solicitud_id):
+
+    solicitud = get_object_or_404(
+        SolicitudIntercambio,
+        id=solicitud_id
+    )
+
+    solicitud.estado_solicitud = 'cancelada'
+
+    solicitud.save()
+
+    return redirect('lista_solicitudes')
