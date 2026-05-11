3731b6e3a39dabe5103321dd78cf619ffb127ced
Androide4 <santi3mayo@gmail.com>
2026-05-11 14:57:05 -0500

    app interacciones, otras configuraciones

--- mercadito/mercadito/settings.py
+++ mercadito/mercadito/settings.py
@@ -59,10 +59,11 @@ ROOT_URLCONF = 'mercadito.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [BASE_DIR / 'templates'],   # ← carpeta templates/ en la raíz
+        'APP_DIRS': True,                   # ← permite encontrar templates dentro de cada app
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -123,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
